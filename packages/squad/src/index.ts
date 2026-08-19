dbbe7a8264d78fdb46cc2e29ce24d302be80d893
Shokk221 <faikemresari5@gmail.com>
2026-08-19 22:57:04 +0100

    faz 4 part 1

--- packages/squad/src/index.ts
+++ packages/squad/src/index.ts
@@ -1,5 +1,6 @@
 export * from './engine.js';
 export * from './plugin.js';
 export * from './adapter.js';
+export * from './scoreboard.js';
 export * from './dev-fixture-engine.js';
 export * from './real-engine-adapter.js';
